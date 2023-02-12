99894b4a5acf086e199b60f281c484ecd57eafc9
alexander <kuznetsov_alx@mail.ru>
2023-02-12 13:48:25 +0300

    добавил ф-ю multi, новую переменную и её вывод

--- new_test_package/plus.py
+++ new_test_package/plus.py
@@ -6,7 +6,16 @@ class CalcPlus:
         self.summa = s
 
 
+    def multi(self, *args):
+        p = 1
+        for i in args:
+            p *= i
+        self.multi = p
+
+
 num = CalcPlus(2, 3, 3, 3, 2, 7)
 num1 = CalcPlus(5, 6, 8, 9, 3, 11, 56, 55)
+num2 = CalcPlus(4, 8, 8, 4)
 print(num.summa)
 print(num1.summa)
+print(num2.summa)
