357c14316768b52c7a289ed1c771d3ffcbe7ebbc
alexander <kuznetsov_alx@mail.ru>
2023-02-12 19:09:38 +0300

    перенес функцию multi в init

--- new_test_package/plus.py
+++ new_test_package/plus.py
@@ -1,16 +1,13 @@
 class CalcPlus:
     def __init__(self, *args):
         s = 0
-        for i in args:
-            s += i
-        self.summa = s
-
-
-    def multi(self, *args):
         p = 1
         for i in args:
+            s += i
             p *= i
+        self.summa = s
         self.multi = p
+
 
 
 num = CalcPlus(2, 3, 3, 3, 2, 7)
@@ -19,4 +16,4 @@ num2 = CalcPlus(4, 8, 8, 4)
 print(num.summa)
 print(num1.summa)
 print('нах')
-print(num2.summa)
+print(num2.multi)
